d49534feb9f4e83d9947aadf2b279173bf52b921
Bridget01 <68750439+Bridget01@users.noreply.github.com>
2020-08-11 11:25:05 -0800

    my third assignment

---
+++ precache-manifest.3af1b722b2c4204416d0a3b5efc6d2de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246215af87cd5ad30425d22c47f590df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0409b25658db9bba9ac9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc167d374e30ea093be2",
+    "url": "/static/js/2.2ace65a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2ace65a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0409b25658db9bba9ac9",
+    "url": "/static/js/main.6f6eb339.chunk.js"
+  },
+  {
+    "revision": "10f705354e75f48e71da",
+    "url": "/static/js/runtime-main.20cf280e.js"
+  }
+]);
